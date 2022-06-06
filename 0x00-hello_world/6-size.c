f908cf76bfd43e5b8999e6782c1e89e2d1ba6356
obineric <obineric@gmail.com>
2022-06-05 23:50:59 -0700

    hullo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s));
+	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)), sizeof(char),sizeof(int), sizeof(long), sizeof(long long), sizeof(float);
 	return (0);
 }
