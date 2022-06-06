48a118bdb414b0f36fe15f93012fd07314258c77
obineric <obineric@gmail.com>
2022-06-06 08:33:42 -0700

    hello

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char str[27] = "abcdefghijklmnopqrstuvwxyz"
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n = 0; n < 26; n++)
+		putchar(str[n]);
+		putchar("\n");
+	return (0);
+}
