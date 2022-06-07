341d90fafedf24d94e63842a4e708f284f1a9efa
obineric <obineric@gmail.com>
2022-06-07 22:11:13 +0100

    hello

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * Function to print _putchar
+ *
+ * Return: 0
+ *
+ */
+
+void printchar (void)
+{
+	char c[9] = "_putchar";
+	int n
+
+	for (n=0; n<9; n++)
+	{
+		_putchar(c[n])
+	}
+}
