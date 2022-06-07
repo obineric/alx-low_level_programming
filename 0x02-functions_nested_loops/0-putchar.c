f8a12a3d07aabb1dba8446883220c0ba77083e3b
obineric <obineric@gmail.com>
2022-06-07 13:47:04 +0100

    hello

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("_putchar");
+	return (0);
+}
