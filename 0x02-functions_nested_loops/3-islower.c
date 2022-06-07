b956e03b7ec884c32eb9d3cff7b56866c5340aff
obineric <obineric@gmail.com>
2022-06-07 16:32:38 -0700

    hello

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _islower: program designed to test if the input is a lowercase letter
+ * _islower - program designed to test if the input is a lowercase letter
  *
  * Return: 1 (successful), 0 (others)
  *
  * @c: the charcter to test
  *
- * Decription: checks to see if the input is a lowercase alphabet
+ * Decription - checks to see if the input is a lowercase alphabet
  */
 int _islower(int c)
 {
