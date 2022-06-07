7b1ba5939dfa07dd0800bb9b627bf42db6dc2a17
obineric <obineric@gmail.com>
2022-06-07 16:17:21 -0700

    hello

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * _islower: program designed to preturn the lesser of two values after comparism
+ *
+ * Return: 1 (successful), 0 (others)
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 97)
+	{
+		if c (<= 122)
+		{
+			return 1;
+		}
+	}
+	else
+	{
+		return 0;
+	}
+}
