60400dc41efebe0a03c95ec1f968807e11555e76
obineric <obineric@gmail.com>
2022-06-07 16:27:21 -0700

    hello

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _islower: program designed to preturn the lesser of two values after comparism
+ * _islower: program designed to test if the input is a lowercase letter
  *
  * Return: 1 (successful), 0 (others)
  *
  */
 int _islower(int c)
 {
+	\* the input c is checked to see if it is a lowercase letter
 	return (c >= 97 && c <= 122);
 }
