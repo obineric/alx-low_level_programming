d256240665542c78ba3120e57e66a65644f49eb7
obineric <obineric@gmail.com>
2022-06-07 16:40:37 -0700

    hello

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - program designed to test if the input is an aplhabet
+ *
+ * Return: 1 (successful), 0 (others)
+ *
+ * @c: the charcter to test
+ *
+ * Decription - checks to see if the input is an alphabet
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122) || (c >= 65 && c<= 90);
+}
