e4e3da5da11dca701aa289c3275984ff3b49f4a5
obineric <obineric@gmail.com>
2022-06-08 11:52:06 +0100

    hello

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ * add - Program to add two numbers
+ *
+ * @n: integer input
+ *
+ * @j: second integer input
+ *
+ * Return: returns 0
+ */
+int add(int n, int j)
+{
+	int result;
+
+	result = n + j;
+
+	_printchar('0' + result);
+}
