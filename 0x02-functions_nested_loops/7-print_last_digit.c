8079a4f001ad4acf73f8b438b6fedf40ffbf3048
obineric <obineric@gmail.com>
2022-06-07 18:17:05 -0700

    hello

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,12 +3,14 @@
 /**
  * print_last_digit - Program to output last digit of a number
  *
+ *@n: integer
+ *
  * Return: last digit
  */
 int print_last_digit(int n)
 {
 	int last_digit;
-	
+
 	if (n < 0)
 	{
 		last_digit = -1 * (n % 10);
