2f4ced47480ad560e4247a247e392bbc29818995
obineric <obineric@gmail.com>
2022-06-07 18:13:41 -0700

    hello

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,16 @@
  */
 int print_last_digit(int n)
 {
-	int rem = n % 10;
-	_putchar(rem + '0');
-	return (rem);
+	int last_digit;
+	
+	if (n < 0)
+	{
+		last_digit = -1 * (n % 10);
+	}
+	else
+	{
+		last_digit = n % 10;
+	}
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
