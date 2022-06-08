1d9d971b8158225ca3daa61ae64f0c782beeb452
obineric <obineric@gmail.com>
2022-06-08 11:31:46 +0100

    hello

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,8 +24,7 @@ void times_table(void)
 				{
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(result + '0');
-					_putchar('$');
+					_putchar(result + '0');;
 					_putchar('\n');
 				}
 				else
