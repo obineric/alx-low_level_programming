af5c23a770ecdb6650f5101d28a4d9354fddffd5
obineric <obineric@gmail.com>
2022-06-08 21:20:29 +0100

    hello

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,10 +4,14 @@
  * main - tests function that prints if integer is positive or negative
  *
  * Return: 0
- *
  */
+
 int main(void)
 {
-	positive_or_negative(98);
+	int i;
+	
+	i = 98;
+	positive_or_negative(i);
+
 	return (0);
 }
