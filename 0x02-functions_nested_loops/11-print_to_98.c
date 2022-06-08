b762bafb2fc9564405c1123fb56946943a79a14c
obineric <obineric@gmail.com>
2022-06-08 13:36:36 +0100

    hello

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints series of numbers till 98
+ *
+ *@n: integer input
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		int i;
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+	else
+	{
+		int j;
+		for (j = n, j > 98; j--)
+		{
+			printf("%d, ", j);
+		}
+	}
+}
+
+
