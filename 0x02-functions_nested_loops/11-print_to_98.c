7ef0c8f3c98a2908bc14265f346207caef520ac5
obineric <obineric@gmail.com>
2022-06-08 13:54:01 +0100

    hello

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,11 +13,11 @@ void print_to_98(int n)
 	if (n < 98)
 	{
 		int i;
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
 			printf("%d, ", i);
 		}
-		_putchar('\n');
+		printf("98\n");
 	}
 	else if (n == 98)
 	{
@@ -26,11 +26,11 @@ void print_to_98(int n)
 	else
 	{
 		int j;
-		for (j = n; j >= 98; j--)
+		for (j = n; j > 98; j--)
 		{
 			printf("%d, ", j);
 		}
-		_putchar('\n');
+		printf("98\n");
 	}
 }
 
