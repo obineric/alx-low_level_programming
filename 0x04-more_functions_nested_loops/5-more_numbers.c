958b93ad1f2f3761b23bbc8fff9e68e2aff0b9f4
obineric <obineric@gmail.com>
2022-06-09 15:05:48 +0100

    hello

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,8 +17,8 @@ void more_numbers(void)
 		{
 			if (j > 9)
 			{
-				_putchar('0' + (j / 10));
-				_putchar('0' + (j % 10));
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
 			}
 			else
 			{
