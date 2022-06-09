c5cb7a04ca470ee1103ffa6cecd2d036c92a509a
obineric <obineric@gmail.com>
2022-06-09 14:22:00 +0100

    hello

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints digits 1 to 14 ten times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int j;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			else
+			{
+				_putchar(j + '0');
+			}
+		}
+	}
+}
