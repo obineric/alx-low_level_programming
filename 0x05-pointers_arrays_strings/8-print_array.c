d43d6ade853be918306a9892acb2319591b53eab
obineric <obineric@gmail.com>
2022-06-13 22:29:21 +0100

    hello

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_array - program to print the members of an array
+ *
+ * @a: input array
+ *
+ * @n: number of input to print
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i <  n-1)
+		{
+			_putchar(a[i] + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar(a[i] + '0');
+		}
+		_putchar('\n');
+	}
+}
