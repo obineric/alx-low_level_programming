97c331e0a7547f3e7ee422d7d6ee1fc205796b55
obineric <obineric@gmail.com>
2022-06-13 21:00:31 +0100

    hello

---
+++ 0x05-pointers_arrays_strings/5-rev-string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * print_rev - prints letters in reverse
+ *
+ * @s: string input
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
