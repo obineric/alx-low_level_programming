74d71b64cb6691f604cdbeb44fd0e53675bccb80
obineric <obineric@gmail.com>
2022-06-13 07:12:55 -0700

    hello

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - program to calculate length of a string
+ *
+ * @s: input strength
+ *
+ * Retuen: void
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
