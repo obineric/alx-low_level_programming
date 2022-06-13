78e07f3c3c22b7bc87d0578ded512ca6e90aa5fb
obineric <obineric@gmail.com>
2022-06-13 07:31:46 -0700

    hello

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - program to print a string and a new line
+ *
+ * @str: string input
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
