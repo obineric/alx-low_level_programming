17d122984cdb9b51b7461eb4244242f486dd2d15
obineric <obineric@gmail.com>
2022-06-13 06:21:58 -0700

    hello

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - program to reset pointer value to 98
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+scripts showing pointer use and application
