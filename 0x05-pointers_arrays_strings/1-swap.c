928fc9a927ba10766c481b856e6e669539288b71
obineric <obineric@gmail.com>
2022-06-13 07:01:53 -0700

    hello

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_int - program to swap the value of two intergers
+ *
+ * @a: input 1
+ *
+ * @b: input 2
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tempa, tempb;
+
+	tempa = *b;
+	tempb = *a;
+	*b = tempb;
+	*a = tempa;
+}:
