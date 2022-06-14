ae90e6708966172034d25bbc71fb54ed5c372a55
obineric <obineric@gmail.com>
2022-06-14 14:35:01 +0100

    hello

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcpy - program to copy string value of a pointer to another variable
+ *
+ * @dest: destination of copy
+ *
+ * @src: source
+ */
+
+char _strcpy(char *dest, char *src)
+{
+	*dest = src;
+	return (dest);
+}
+
