8b3ff3d4e12658250ccb9e1feb7320a45bf328d7
obineric <obineric@gmail.com>
2022-06-16 15:09:19 +0100

    hello

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,7 +15,7 @@ char _strcat(char *dest, char *src)
 	int dest_len = strlen(dest);
 	int src_len = strlen(src);
 	int i;
-	
+
 	for (i = 0, i < src_len && src[i] != '\0'; i++)
 	{
 		dest[dest_len + i] = src[i];
