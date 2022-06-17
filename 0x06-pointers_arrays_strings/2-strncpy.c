b90948801ee165f079be1f71c12291a99127086d
obineric <obineric@gmail.com>
2022-06-17 11:13:34 +0100

    hello

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - program to copy one string to another
+ *
+ * @dest: destination input
+ *
+ * @src: source input
+ *
+ * @n: input to show limit
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (*(dest + i) == '\0')
+		{
+			*(dest + i) = '\0';
+		}
+		else
+		{
+			*(dest + i) = *(src + i);
+		}
+		i++;
+	}
+}
