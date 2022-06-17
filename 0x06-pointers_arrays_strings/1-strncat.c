e347acdbe94b1d7c311d6d235eee5cbbcfde2bae
obineric <obineric@gmail.com>
2022-06-17 10:23:03 +0100

    hello

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncat - program to concatenate two strings up to a point n
+ *
+ * @dest: destination input
+ *
+ * @src: source input
+ *
+ * @n: point at which copy ends
+ *
+ * Return: concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+
+	while (*(dest + i) != '\0')
+		i++;
+
+	j = 0;
+
+	while (*(src + j) != '\0' && j <= n)
+	{
+		*(dest + i + j) = *(src + j);
+		j++;
+	}
+
+	return (dest);
+}
