cc4afd748e8765f326e0246e1c12a609d3ae0d5d
obineric <obineric@gmail.com>
2022-06-17 12:11:10 +0100

    hello

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337
+ *
+ * @s: The string input
+ * Return: void
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char *a = "AaEeOoTtLl";
+	char *b = "4433007711";
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(a + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(a + j))
+				*(s + i) = *(b + j);
+		}
+	}
+	return (s);
+}
+© 2022 GitHub, Inc.
+Terms
+
