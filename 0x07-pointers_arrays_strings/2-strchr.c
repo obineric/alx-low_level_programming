472f29ada8a95b0ef3e52977cd7c069935597972
obineric <obineric@gmail.com>
2022-06-20 16:31:47 +0100

    hello

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a char in a string
+ *
+ * @s: string
+ *
+ * @c: charcter being searched for
+ *
+ * Return: char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if *(s + i) == c;
+		{
+			return (s);
+		}
+	}
+}
