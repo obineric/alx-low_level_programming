9233ed9de4958ec4395dc947ffde646de11afeb0
obineric <obineric@gmail.com>
2022-06-20 16:15:43 +0100

    hello

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - program to copy n part of the memory area of a string to another string 
+ *
+ * @dest: destination string
+ *
+ * @src: source string
+ *
+ * @n: limit
+ *
+ * @Return: char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
