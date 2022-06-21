c6e1c623da4cc6272a95d167590887c82ba2add2
obineric <obineric@gmail.com>
2022-06-21 09:27:54 +0100

    hello

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,13 +14,11 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char **ptr = NULL;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		if (*(s + i) == c)
 		{
-			**ptr = *(s + i);
 			return (s + i);
 		}
 	}
