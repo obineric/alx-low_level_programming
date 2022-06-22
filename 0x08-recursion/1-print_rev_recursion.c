295129f402959104f61db01c4965655d62836c66
obineric <obineric@gmail.com>
2022-06-22 20:58:19 +0100

    hello

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Program to pring a string backwards
+ * @s: string input
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	unsigned int length;
+	length = sizeof(s) - 1;
+
+	if (length == 0)
+	{
+		_putchar(s[0]);
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[length - 1]);
+		_print_rev_recursion(s - 1);
+	}
+}
