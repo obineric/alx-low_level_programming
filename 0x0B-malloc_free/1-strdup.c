c6ef4841757796e7bf41c8e4177bfb40700e399c
obineric <obineric@gmail.com>
2022-06-27 21:45:18 +0100

    hello

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - program to copy a string to a memory location
+ * @str: string input
+ * Return: array containing copied string
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i;
+
+	if (*str == NULL)
+		return (NULL);
+	if (!ptr = malloc(*str))
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = str[i];
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
