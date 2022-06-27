4bb73c4dc55a6f76e7dfbf60da44f6e3064f5e5f
obineric <obineric@gmail.com>
2022-06-27 23:05:46 +0100

    hello

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -40,6 +40,8 @@ char *str_concat(char *s1, char *s2)
 	size_s1 = _strlen(s1);
 	size_s2 = _strlen(s2);
 	ptr =  realloc(s1, (size_s1 + size_s2 + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; i < size_s2; i++)
 	{
 		*(ptr + size_s1 + i) = *(s2 + i);
