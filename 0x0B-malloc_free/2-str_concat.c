b38556db4288cf9b8eac400fd05e4ddb9081cf7c
obineric <obineric@gmail.com>
2022-06-27 22:47:14 +0100

    hello

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - int _strlen(char *s)
+ * @s1: input string 1
+ * @s2: input string 2
+ * Return: concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	unsigned int size_s1, size_s2;
+	int i;
+
+	size_s1 = _strlen(s1);
+	size_s2 = _strlen(s2);
+	ptr =  realloc(s1, (size_s1 + size_s2) * sizeof(char));
+	for (i = 0, i < size; i++)
+	{
+		*(ptr + size_s1 + i) = *(s2 + i);
+	}
+	*(s1 + size_s1 + i) = '\0';
+	return (s1);
+}
