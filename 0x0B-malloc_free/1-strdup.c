53d0181b5e8f2fc8d99c1ce392c4d9a3f4280b6e
obineric <obineric@gmail.com>
2022-06-27 21:51:23 +0100

    hello

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,12 +15,13 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	if ((!ptr == malloc(*str)))
+	if (ptr == malloc(*str))
+		for (i = 0; i < size; i++)
+		{
+			ptr[i] = str[i];
+		}
+	else
 		return (NULL);
-	for (i = 0; i < size; i++)
-	{
-		ptr[i] = str[i];
-	}
 	ptr[i] = '\0';
 	return (ptr);
 }
