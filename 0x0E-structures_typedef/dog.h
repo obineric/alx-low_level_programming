bec9bb12cb9c6e007cc82c5e0d8d386b2a31e4ed
obineric <obineric@gmail.com>
2022-07-06 22:21:48 +0100

    hello

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,37 @@
-#ifndef DOG_H 
- #define DOG_H 
- /** 
-  * struct dog - dog data 
-  * @name: name of the dog 
-  * @age: age of the dog 
-  * @owner: name of the owner of the dog 
-  * 
-  * Description: Defines metadata about the dog and type of dog's struct 
-  */ 
- typedef struct dog 
- { 
-         char *name; 
-         float age; 
-         char *owner; 
- } dog_t; 
- /* Additional function prototypes */ 
- int _putchar(char c); 
- void init_dog(struct dog *d, char *name, float age, char *owner); 
- void print_dog(struct dog *d); 
- dog_t *new_dog(char *name, float age, char *owner); 
- void free_dog(dog_t *d); 
- #endif
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A general dog struct
+ *
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: the dog owner
+ *
+ * Description - A dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
+#ifndef HEADER_H
+#define HEADER_H
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
+
