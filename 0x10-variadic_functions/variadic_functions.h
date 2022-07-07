59f5e4e1efb8725ef180f9b1fc3a3f62cd4f6aa2
obineric <obineric@gmail.com>
2022-07-07 20:27:55 +0100

    hello

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNC_H
+#define FUNC_H
+#include <stdlib.h>
+
+int _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
