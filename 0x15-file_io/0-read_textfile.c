a247cdc619d234bb703d120033b9dabf793a7b15
obineric <obineric@gmail.com>
2022-07-25 16:19:05 +0100

    hello

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,40 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include "main.h"
+
+/**
+ * read_textfile - read a text file and prints it to the POSIX
+ * standard output
+ * @filename: file name
+ * @letters: number of leters to be read and printed
+ * Return: the number of letters that was read and printed
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char buff[letters];
-	int count = 0;
+	char *buff;
+	ssize_t count_r, count_w;
 
-	fd = open("(*filename)", O_RDONLY);
+	if (fd == NULL)
+		return (0);
 
+	fd = open(filename, O_RDWR);
 	if (fd == -1)
 		return (0);
-	read((*filename), buff, letters);
-	buff[letters] = '\0';
+	
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	count_r = read(fd, buff, letters);
+	if (count_r == -1)
+		return (0);
+
+	count_w = write(STDOUT_FILENO, buff, count_r);
+	if (count_w == -1 || count_r != count_w)
+		return (0);
+
+	free(buffer);
 
 	close(fd);
 
-	while (*(buff))
-	{
-		_putchar(*buff);
-		buff++;
-		count++;
-	}
+	return (count_w);
