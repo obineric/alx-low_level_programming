45eb24f53555fb8650961f9f41aaf79c2f7baf96
obineric <obineric@gmail.com>
2022-07-25 16:35:16 +0100

    hello

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDWR);
 	if (fd == -1)
 		return (0);
-	
+
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 		return (0);
